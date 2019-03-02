5389e89b127dda167d3177ce33770ec886df630f
Toni <toni.turpeinen@helsinki.fi>
2019-03-02 21:27:48 +0200

    .

--- newtest/topicidtest.py
+++ newtest/topicidtest.py
@@ -1,6 +1,3 @@
-#selvitä miten saa toimimaan tfidf kirjastolla suoraan.
-#IDEA: laita topic id feature_nameihin:
-
 import csv
 import numpy as np
 import string
@@ -13,21 +10,19 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import NMF
 from sklearn.feature_extraction import text
 
-#from stop_words import get_stop_words
-#finnishStops = get_stop_words('finnish')
-
-#text_sect = 7
+#text_sect is 7
+#text_sub is 8
 
 inputFile = open("stopwords-fi.json", encoding='UTF-8')
 finnishStops = json.load(inputFile, encoding='UTF-8')
 
-moreStopWords = ["edes", "myös", "http", "www"]
+moreStopWords = ["edes", "myös", "http", "www", "sä", "mä", "voida", "com", "esim"]
 
 customStops = text.ENGLISH_STOP_WORDS.union(finnishStops)
 customStops = customStops.union(moreStopWords)
 
-res = []
-sections = []
+lemmaStrings = []
+realTopics = []
 
 path = "./aineisto/"
 for filename in os.listdir(path):
@@ -38,24 +33,50 @@ for filename in os.listdir(path):
             if header == 0:
                 header += 1
                 continue
-            res.append(row[3])
-            sections.append(row[7])
+            if not (row):
+                continue
+            lemmaStrings.append(row[3])
+            realTopics.append(row[7])
 
-##max_df = 0.9, min_df=2,
-vectorizer = TfidfVectorizer(analyzer='word', stop_words = customStops)
+vectorizer = TfidfVectorizer(analyzer='word', stop_words = customStops, max_df = 0.9, min_df = 10)
 
-X = vectorizer.fit_transform(res)
-#print("X shape: ")
-#print(X.shape)
+numberOfTopics = len(set(realTopics))
+
+X = vectorizer.fit_transform(lemmaStrings)
+print("X shape: ")
+print(X.shape)
 
 words = np.array(vectorizer.get_feature_names())
 
-nmf = NMF(n_components=2, solver='mu')
+nmf = NMF(n_components=numberOfTopics, solver='mu')
 W = nmf.fit_transform(X)
 H = nmf.components_
 
 for i, topic in enumerate(H):
-	print("Topic {}: {}".format(i + 1, ",".join([str(x) for x in words[topic.argsort()[-15:]]])))
+    print("Aihe {}: {}".format(i + 1, ",".join([str(x) for x in words[topic.argsort()[::-1][:15]]])))
 
-print(sections)
-input()
+topicList = []
+
+print("Mahdolliset keskustelualueet: ")
+print(set(realTopics))
+
+for i, topic in enumerate(H):
+    givenTopic = input("Anna keskustelualue aiheelle " + str(i+1) + ": ")
+    topicList.append(givenTopic)
+
+predictedTopics = []
+
+for array in W:
+    topic = topicList[list(array).index(max(array))]
+    predictedTopics.append(topic)
+
+hit, miss = 0, 0
+
+for i, pTopic in enumerate(predictedTopics):
+    if pTopic == realTopics[i]:
+        hit += 1
+    else:
+        miss += 1
+
+print("Osumaprosentti:")
+print(round(hit/(hit+miss), 2))
